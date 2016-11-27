3035e536a078ac3413c535f36c565d224c048574
aldentea <aldentea-dev@otegami.aldente.mydns.jp>
2016-11-27 12:50:40 +0900

    GrandMutusDataを0.8.4にアップグレード。(0.3.3.1)

--- SweetMutusData/Properties/AssemblyInfo.cs
+++ SweetMutusData/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SweetMutusData")]
-[assembly: AssemblyCopyright("Copyright © aldentea 2015")]
+[assembly: AssemblyCopyright("Copyright © aldentea 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.3.0")]
-[assembly: AssemblyFileVersion("0.3.3.0")]
+[assembly: AssemblyVersion("0.3.3.1")]
+[assembly: AssemblyFileVersion("0.3.3.1")]
