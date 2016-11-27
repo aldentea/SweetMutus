fddfc4248935cc6dcd5a9debf23ae297ba830d82
aldentea <aldentea-dev@otegami.aldente.mydns.jp>
2016-11-27 19:49:35 +0900

    一部メソッドをpublicに変更。その他、細かい部分を修正。(0.2.3)

--- SweetMutus/Properties/AssemblyInfo.cs
+++ SweetMutus/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SweetMutus")]
-[assembly: AssemblyCopyright("Copyright © aldentea 2015")]
+[assembly: AssemblyCopyright("Copyright © aldentea 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.2.1")]
-[assembly: AssemblyFileVersion("0.2.2.1")]
+[assembly: AssemblyVersion("0.2.3.0")]
+[assembly: AssemblyFileVersion("0.2.3.0")]
